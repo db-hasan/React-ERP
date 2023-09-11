c06d4cd553acac462e0559dd0b7e75cfeccae949
db-hasan <infoalihasanbd@gmail.com>
2023-09-12 00:45:00 +0600

    dashboard

--- src/Backend/Dashboard.jsx
+++ src/Backend/Dashboard.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './style.css'
+// import { Link } from 'react-router-dom'
+import { Scrollbars } from 'react-custom-scrollbars-2';
 import BackHeader from './BackHeader'
 import BackFooter from './BackFooter'
 import Sidebar from './Sidebar'
@@ -14,38 +16,42 @@ const Dashboard = () => {
           <Sidebar />
         </div>
         <div className="col-sm-8 col-md-9 col-lg-10 dash">
+          <Scrollbars autoHeight
+            autoHeightMin={100}
+            autoHeightMax={500}>
           <div className='px-4'>
             <div className='row pt-2'>
               <div className='col-md-3 p-2'>
                 <div className='card p-3 text-light bg-info bg-gradient'>
                   <p><strong>Sales Order</strong></p>
                   <p><i className='fa-solid fa-cart-shopping fa-2xl'></i> 1011001</p>
-                  <span>Total Stock</span>
+                  <span>Sales Price</span>
                 </div>
               </div>
               <div className='col-md-3 p-2'>
                 <div className='card p-3 text-light bg-success bg-opacity-75'>
-                  <p><strong>Expenses</strong></p>
+                  <p><strong>Profit</strong></p>
                   <p><i className='fa-solid fa-money-check-dollar fa-2xl'></i> 1011001</p>
-                  <span>Total Stock</span>
+                  <span>Profit Amount</span>
                 </div>
               </div>
               <div className='col-md-3 p-2'>
                 <div className='card p-3 text-light bg-warning bg-opacity-75'>
-                  <p><strong>Profit</strong></p>
-                  <p><i className='fa-solid fa-industry fa-2xl'></i> 1011001</p>
-                  <span>Total Stock</span>
+                  <p><strong>Expenses</strong></p>
+                  <p><i className='fa-solid fa-cart-flatbed-suitcase fa-2xl'></i> 1011001</p>
+                  <span>Expenses Cost</span>
                 </div>
               </div>
               <div className='col-md-3 p-2'>
                 <div className='card p-3 text-light bg-danger bg-opacity-75'>
-                  <p><strong>Total Stock</strong></p>
-                  <p><i className='fa-solid fa-cart-flatbed-suitcase fa-2xl'></i> 1011001</p>
-                  <span>Total Stock</span>
+                  <p><strong>Gross Profit</strong></p>
+                  <p><i className='fa-solid fa-industry fa-2xl'></i> 1011001</p>
+                  <span>Profit Balance</span>
                 </div>
               </div>
             </div>
           </div>
+          </Scrollbars>
         </div>
       </div>
       <BackFooter />
