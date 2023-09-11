798d5d5941f7e5124e48bd2fdb151189c377a752
db-hasan <infoalihasanbd@gmail.com>
2023-09-11 11:13:51 +0600

    updt

--- src/Backend/Pages/Stock/RetrunStockInvioce.jsx
+++ src/Backend/Pages/Stock/RetrunStockInvioce.jsx
@@ -19,82 +19,84 @@ const RetrunStockInvioce = () => {
                         <div className='col text-end icons'>
                             <div className="btn-group" role="group" aria-label="Basic mixed styles example">
                                 <button className="btn btn-primary"><Link to="/RetrunStock" className="text-light"><i className="fa-solid fa-plus" /> Add New </Link></button>
-                            </div>    
-                        </div>                           
+                            </div>
+                        </div>
                     </div>
                     <hr />
                     <div className='row'>
                         <div className='col'><h3>Manage</h3></div>
                         <div className='col text-end icons  pb-2'>
-                                <div className="btn-group" role="group" aria-label="Basic mixed styles example">
-                                    <button type="button" className="btn btn-success">View</button>
-                                    <button type="button" className="btn btn-warning">Print</button>
-                                </div>
+                            <div className="btn-group" role="group" aria-label="Basic mixed styles example">
+                                <button type="button" className="btn btn-success">View</button>
+                                <button type="button" className="btn btn-warning">Print</button>
                             </div>
                         </div>
-                        <div className='row show_search'>
-                            <div className="col">
-                                <span className='m-2' >Show </span>
-                                <select className='px-3' name="" id="">
-                                    <option value="">10</option>
-                                    <option value="">25</option>
-                                    <option value="">50</option>
-                                    <option value="">100</option>
-                                </select>
-                                <span className='m-2' > Entries</span>
-                            </div>
-                            <div className="col">
-                                <div className="input-group ">
-                                    <input type="text" className="form-control" placeholder="Type here" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-                                    <span className="input-group-text" id="basic-addon2">Search</span>
-                                </div>
+                    </div>
+                    <div className='row show_search'>
+                        <div className="col">
+                            <span className='m-2' >Show </span>
+                            <select className='px-3' name="" id="">
+                                <option value="">10</option>
+                                <option value="">25</option>
+                                <option value="">50</option>
+                                <option value="">100</option>
+                            </select>
+                            <span className='m-2' > Entries</span>
+                        </div>
+                        <div className="col">
+                            <div className="input-group ">
+                                <input type="text" className="form-control" placeholder="Type here" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                                <span className="input-group-text" id="basic-addon2">Search</span>
                             </div>
                         </div>
-                        <Scrollbars autoHeight
-                            autoHeightMin={100}
-                            autoHeightMax={300}>
-                            <table className="table">
-                                <thead>
-                                    <tr>
-                                        <th><a href=""><i className="fa-solid fa-right-left fa-rotate-90 fa-sm" ></i></a> ID</th>
-                                        <th><a href=""><i className="fa-solid fa-right-left fa-rotate-90 fa-sm" ></i></a> Suppliers</th>
-                                        <th><a href=""><i className="fa-solid fa-right-left fa-rotate-90 fa-sm" ></i></a> Status</th>
-                                        <th>Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>1111</td>
-                                        <td>Amazone</td>
-                                        <td>Active</td>
-                                        <td className="icons">
-                                            <a href="" className="view" data-toggle="modal"><i className="fa-solid fa-eye"></i></a>
-                                            <a href="" className="edit" data-toggle="modal"><i className="fa-solid fa-pen"></i></a>
-                                            <a href="" className="print" data-toggle="modal"><i class="fa-solid fa-print"></i></a>
-                                            <a href="" className="delete" data-toggle="modal"><i className="fa-solid fa-trash"></i></a>
-                                        </td>
-                                    </tr>
-                                </tbody>
+                    </div>
+                    <Scrollbars autoHeight
+                        autoHeightMin={100}
+                        autoHeightMax={300}>
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th><a href=""><i className="fa-solid fa-right-left fa-rotate-90 fa-sm" ></i></a> ID</th>
+                                    <th><a href=""><i className="fa-solid fa-right-left fa-rotate-90 fa-sm" ></i></a> Suppliers</th>
+                                    <th><a href=""><i className="fa-solid fa-right-left fa-rotate-90 fa-sm" ></i></a> Phone</th>
+                                    <th><a href=""><i className="fa-solid fa-right-left fa-rotate-90 fa-sm" ></i></a> Date</th>
+                                    <th><a href=""><i className="fa-solid fa-right-left fa-rotate-90 fa-sm" ></i></a> SalesMan</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1111</td>
+                                    <td>Hasan</td>
+                                    <td>01723629080</td>
+                                    <td>11/09/2023</td>
+                                    <td>Mim</td>
+                                    <td className="icons">
+                                        <a href="" className="edit" data-toggle="modal"><i className="fa-solid fa-pen"></i></a>
+                                        <a href="" className="view" data-toggle="modal"><i class="fa-solid fa-file-invoice fa-lg"></i></a>
+                                    </td>
+                                </tr>
+                            </tbody>
 
-                            </table>
-                        </Scrollbars>
-                        <div className=''>
-                            <nav aria-label="...">
-                                <ul className="pagination">
-                                    <li className="page-item disabled">
-                                        <a className="page-link">Previous</a>
-                                    </li>
-                                    <li className="page-item"><a className="page-link" href="#">1</a></li>
-                                    <li className="page-item active" aria-current="page">
-                                        <a className="page-link" href="#">2</a>
-                                    </li>
-                                    <li className="page-item"><a className="page-link" href="#">3</a></li>
-                                    <li className="page-item">
-                                        <a className="page-link" href="#">Next</a>
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
+                        </table>
+                    </Scrollbars>
+                    <div className=''>
+                        <nav aria-label="...">
+                            <ul className="pagination">
+                                <li className="page-item disabled">
+                                    <a className="page-link">Previous</a>
+                                </li>
+                                <li className="page-item"><a className="page-link" href="#">1</a></li>
+                                <li className="page-item active" aria-current="page">
+                                    <a className="page-link" href="#">2</a>
+                                </li>
+                                <li className="page-item"><a className="page-link" href="#">3</a></li>
+                                <li className="page-item">
+                                    <a className="page-link" href="#">Next</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
             </div>
             <BackFooter />
